0d6c8ae87102014e623a21071e5111b42cef61c9
Juris Gribuska <juris.gribuska@bt.com>
2021-06-28 13:28:57 +0300

    completed

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,16 +1,31 @@
 class Fixnum
   VALUES = {
     1000 => "M",
+    900 => "CM",
     500 => "D",
+    400 => "CD",
     100 => "C",
+    90 => "XC",
     50 => "L",
+    40 => "XL",
     10 => "X",
+    9 => "IX",
     5 => "V",
+    4 => "IV",
     1 => "I"
   }
 
 
   def to_roman
-    VALUES[self]
+    output = []
+    num = self
+
+    VALUES.keys.each do |key|
+      while((num - key) >= 0)
+        num = num - key
+        output << VALUES[key]
+      end
+    end
+    output.join
   end
 end
